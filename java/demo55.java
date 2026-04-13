2de79a956db12214c681637bb8667722766f7936
nandan_prathik <nandannandan9689@gmail.com>
2026-04-13 12:36:53 +0530

    Add files via upload

---
+++ java/demo55.java
@@ -0,0 +1,26 @@
+//write a python program to generate prime numbers from 1 to 100
+class demo55
+{
+	public static void main(String args[])
+	{
+		int i,j;
+		for(i=3;i<100;i++)
+		{
+			for(J=2;j<=i-1;j++)
+			{
+				if(i%j==0)
+				{	
+				prime=0;
+				break;
+				}
+			}
+			if(prime=1)
+			{
+				System.out.println(i);
+			}
+		}
+	}
+}
+
+			
+	
